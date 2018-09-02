4b308c3a290e691836660795aa215df3920f5305
jerry <linhuihui225@gmail.com>
2018-09-02 23:46:17 +0800

    adjust activity pao layout

--- app/src/main/java/com/example/jerry/mvvmpractice/view/PaoActivity.kt
+++ app/src/main/java/com/example/jerry/mvvmpractice/view/PaoActivity.kt
@@ -3,6 +3,7 @@ package com.example.jerry.mvvmpractice.view
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import com.example.jerry.mvvmpractice.R
 import com.example.jerry.mvvmpractice.databinding.ActivityPaoBinding
 import com.example.jerry.mvvmpractice.model.remote.PaoService
@@ -10,6 +11,8 @@ import com.example.jerry.mvvmpractice.viewmodel.PaoViewModel
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 
 class PaoActivity : AppCompatActivity() {
 
@@ -24,11 +27,17 @@ class PaoActivity : AppCompatActivity() {
             .baseUrl("http://api.jcodecraeer.com/")
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+                    .build()
+            )
             .build()
             .create(PaoService::class.java)
 
+
         paoViewModel = PaoViewModel(remote)
         binding.vm = paoViewModel
-
+        paoViewModel.loadArticle()
     }
 }
