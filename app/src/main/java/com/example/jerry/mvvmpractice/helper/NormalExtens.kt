9c68404e1344050fec7ceae833a5bcdab3efe3b4
jerry <linhuihui225@gmail.com>
2018-09-30 18:22:58 +0800

    load aritcle from repo

--- app/src/main/java/com/example/jerry/mvvmpractice/helper/NormalExtens.kt
+++ app/src/main/java/com/example/jerry/mvvmpractice/helper/NormalExtens.kt
@@ -1,18 +1,24 @@
 package com.example.jerry.mvvmpractice.helper
 
 import android.arch.lifecycle.LifecycleOwner
+import com.uber.autodispose.AutoDispose
 import com.uber.autodispose.SingleSubscribeProxy
+import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import us.feras.mdv.MarkdownView
 import java.util.concurrent.TimeUnit
 
-fun MarkdownView.setMarkdown(markdownView: String?) {
-    loadMarkdown(markdownView, "file:///android_asset/markdown.css")
+fun MarkdownView.setMarkdown(markdown: String?) {
+    loadMarkdown(markdown, "file:///android_asset/markdown.css")
 }
 
 fun <T> Single<T>.async(withDelay: Long = 0): Single<T> =
     this.subscribeOn(Schedulers.io())
         .delay(withDelay, TimeUnit.MILLISECONDS)
         .observeOn(AndroidSchedulers.mainThread())
+
+fun  <T> Single<T>.bindLifeCycle(owner: LifecycleOwner): SingleSubscribeProxy<T> {
+    return this.`as`(AutoDispose.autoDisposable(AndroidLifecycleScopeProvider.from(owner)))
+}
--- app/src/main/java/com/example/jerry/mvvmpractice/view/PaoActivity.kt
+++ app/src/main/java/com/example/jerry/mvvmpractice/view/PaoActivity.kt
@@ -1,21 +1,25 @@
 package com.example.jerry.mvvmpractice.view
 
+import com.example.jerry.mvvmpractice.R
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.Menu
-import com.example.jerry.mvvmpractice.R
+import android.view.MenuItem
+import android.webkit.WebViewClient
+import android.widget.Toast
 import com.example.jerry.mvvmpractice.databinding.ActivityPaoBinding
+import com.example.jerry.mvvmpractice.helper.bindLifeCycle
 import com.example.jerry.mvvmpractice.model.local.AppDatabase
 import com.example.jerry.mvvmpractice.model.remote.PaoService
 import com.example.jerry.mvvmpractice.model.respository.PaoRepo
 import com.example.jerry.mvvmpractice.viewmodel.PaoViewModel
+import kotlinx.android.synthetic.main.activity_pao.*
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 
 class PaoActivity : AppCompatActivity() {
 
@@ -42,7 +46,33 @@ class PaoActivity : AppCompatActivity() {
         val repo = PaoRepo(remote, local)
         paoViewModel = PaoViewModel(repo)
         binding.vm = paoViewModel
-        paoViewModel.loadArticle()
+
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu?.let {
+            menuInflater.inflate(R.menu.detail_menu, it)
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        item?.let {
+            when (it.itemId) {
+                R.id.action_refresh -> paoViewModel.loadArticle()
+                    .bindLifeCycle(this)
+                    .subscribe({}, { dispatchError(it) })
+                else -> {
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun dispatchError(error: Throwable?, length: Int = Toast.LENGTH_SHORT) {
+        error?.let {
+            it.printStackTrace()
+            Toast.makeText(this, it.message, length).show()
+        }
+    }
 }
